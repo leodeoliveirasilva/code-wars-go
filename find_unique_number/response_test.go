7e7e2285a45732b8e7170441da648aec969d5c52
leodeoliveirasilva <leonardo.oliveirasilva1005@gmail.com>
2020-07-30 13:04:54 -0300

    refactor of test

--- find_unique_number/response_test.go
+++ find_unique_number/response_test.go
@@ -1,13 +1,27 @@
 package find_unique_number
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	. "github.com/pranavraja/zen"
+)
+
+type TestCase struct {
+	test []float32
+	expected float32
+}
 
 func TestFindUnique(t *testing.T) {
-		response := FindUniq([]float32{ 1.0, 1.0, 1.0, 2.0, 1.0, 1.0 })
-		expected := float32(2)
-		if response != expected {
-			t.Error()
-		} else {
-			t.Logf("FIND UNIQUE TEST PASSED")
-		}
+	Desc(t, "Find Unique Simple Tests", func(it It) {
+			var testCaseList = []TestCase {
+				TestCase{[]float32{ 1.0, 1.0, 1.0, 2.0, 1.0, 1.0 }, float32(2)},
+				TestCase{[]float32{ 0, 0, 0.55, 0, 0 }, float32(0.55)},
+			}
+
+			for _, testCase := range testCaseList {
+				it(fmt.Sprintf("should find %g on test array", testCase.expected), func(expect Expect) {
+					expect(FindUniq(testCase.test)).ToEqual(testCase.expected)
+				})
+			}
+	})
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.14
 require (
 	github.com/onsi/ginkgo v1.14.0
 	github.com/onsi/gomega v1.10.1
+	github.com/pranavraja/zen v0.0.0-20140902082244-564f2e04e292
+	github.com/smartystreets/goconvey v1.6.4
 )
